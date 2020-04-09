7b078debf76693b5226ba245cf76d2eac41cb50b
Jakub Solecki <solko12.98@gmail.com>
2020-04-09 21:49:12 +0200

    Changed endpoint in Startup to Controllers for more readable code

---
+++ KPZ-Catering-API/KPZ-Catering-API/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KPZ_Catering_API.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet("")]
+        public string getHelloWorld() {
+            return "Hello World!";
+        }
+    }
+}
